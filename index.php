9bd6612c8e61595c4b90944ed1c199fa709e7f8d
Chris Herms <skimnaples@gmail.com>
2011-12-27 22:55:45 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,140 @@
+<?php
+?>
+<html>
+	<head>
+		<title>CPAP Medical Supplies and Services - Military CPAP Supplies - Jacksonville FL</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name="description" content="CPAP supplies for Medicare beneficiaries, active duty military, retired military and military families.">
+		<style>
+			body {
+				font-family: arial;
+				font-size:15px;
+				color: #333333;
+				background: #333333 url(images/diagmonds.png) repeat 0 0;
+			}
+
+			#container {
+				margin: 1.6em auto;
+				width: 900px;
+				position: relative;
+				border: 1em solid #5a5348;
+				background-color: #ffffff;
+				padding: 2em;
+				-webkit-border-radius: 1em;
+				-moz-border-radius: 1em;
+				border-radius: 1em;
+				-moz-box-shadow: 0px 0px 4em #000000;
+				-webkit-box-shadow: 0px 0px 4em #000000;
+				box-shadow: 0px 0px 4em #000000;
+			}
+			h1 {
+				margin: 0 0 1.5em 0.5em;
+				text-indent: -9999px;
+				background: url(/images/logo.png) no-repeat;
+				width: 400px;
+				height: 81px;
+			}
+			h2 {
+				color: #736a5a;
+				text-shadow:1px 1px 3px #bbbbbb;
+			}
+			#nav {
+				position: absolute;
+				right: 2em;
+				top: 0;
+				width: 220px;
+				height: 2em;
+			}
+				#nav ul {
+					margin: 1em 0;
+					list-style: none;
+				}
+					#nav ul li {
+						margin-bottom: 0.4em;
+					}
+				#nav li.number {
+					text-align: right;
+				}
+			#footer {
+				text-align: center; margin: 5em 0 1em 0;
+			}
+			#footer ul { margin: 0; list-style: none; }
+			#footer li { display: inline; margin: 0 0.5em; }
+			
+			#prescription {
+				color: #ffffff;
+				text-shadow:-1px -1px 1px #333333;
+				background-color: #5a5348;
+				-moz-box-shadow: 0.5em 0.5em 1em #666666;
+				-webkit-box-shadow: 0.5em 0.5em 1em #666666;
+				box-shadow: 0.5em 0.5em 1em #666666;
+				padding: 2em;
+				font-size: 1.3em;
+				-webkit-border-radius: 2em;
+				-moz-border-radius: 2em;
+				border-radius: 2em;
+				border: 0.4em solid #bfb8aa;
+			}
+			
+			img.left { float:left; margin: 0 2em 1em 0; }
+			img.right { float: right; margin: 0 0 1em 2em; }
+			
+			.article { margin-bottom: 2.5em; clear: both; }
+			.center { text-align: center; }
+			.bold { font-weight: bold; }
+		</style>
+	</head>
+	<body>
+		<div id="container">
+			<h1>
+				CPAP Medical Supplies and Services
+			</h1>
+			<div id="nav">
+				<ul>
+					<li class="number"><span class="bold">Phone:</span> 904.281.2727</li>
+					<li class="number"><span class="bold">Fax:</span> 904.281.0444</li>
+					<li class="center bold">Mailing Address:</li>
+					<li>6950 Philips Highway #36<br />Jacksonville, FL 32216</li>
+				</ul>
+			</div>
+			<div id="content">
+				<div class="article">
+					<img src="images/system-one-provider.jpg" alt="Philips Respironics System One Device" width="200px" height="150px" class="right" />
+					<h2>CPAP Medical Supplies specializes in sleep apnea products and services.</h2>
+					We provide compassionate, high quality care that improves the overall health and lifestyle of our patients and their families by working together in a partnership with our physicians and other regional healthcare providers.
+				</div>
+				<div class="article">
+					<h2>Sleep apnea supplies for TRICARE and Medicare beneficiares</h2>
+					We have provided sleep apnea devices to hundreds of active duty service members(ADSM), retired service members, and military family members. We have given each patient personal service and followed up to insure compliance and the patient's well-being.<br /><br />
+					After receiving the referral from the military treatment facility, TRICARE or your physician, we will make an appointment with you, at your convenience. We can visit you in your home(or you can come to our office, if you prefer). The education and setup usually takes around 45 minutes to an hour. We will answer any questions you may have and also supply you with manuals and most importantly, our 24/7 on-call phone number.
+				</div>
+				<div class="article">
+					<img src="images/comfort-gel-blue.jpg" alt="Philips Respironics Comfort Gel Blue CPAP Mask" width="200px" height="150px" class="left" />
+					<h2>Replacement sleep apnea mask, tubing, filters and other accessories.</h2>
+					If you need a replacement mask or other supplies, we provide nationwide shipping and support. <strong>A prescription is required for all sleep apnea supplies and services.</strong>
+				</div>
+				<div id="prescription" class="bold article">
+					A prescription is required because the Food and Drug Administration classifies CPAP masks, accesories, and devices as Class II Medical Devices, which require a prescription.
+				</div>
+				<div class="article">
+					<h2>Our products</h2>
+					The mask that you use with your CPAP is the most important aspect of your sleep apnea therapy. Proper fitting is crucial and is usually the reason for either successful compliance or no compliance.<br /><br />
+					There are hundreds of Interfaces on the market today. Many claim to be the "best" and the most "comfortable". The truth of the matter is that what works for your neighbor may not work for you.<br /><br />
+					We take the time during the fitting process to ask you specific questions concerning your sleeping habits (sleeping on your side) and other information that will help us decide what may work best for you.<br /><br />
+					The most common style nasal Cpap masks are divided into 2 types: a nasal mask that seals over your nose and a nasal pillow mask that seals at the end of your nostrils.
+				</div>
+				<div class="article">
+					<h2>Our location in Jacksonville, Florida</h2>
+					<iframe width="640" height="480" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=CPAP+Medical+Supplies+and+Services,+Inc.,+Jacksonville,+FL&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=60.894251,49.482422&amp;vpsrc=6&amp;ie=UTF8&amp;hq=CPAP+Medical+Supplies+and+Services,+Inc.,&amp;hnear=Jacksonville,+Duval,+Florida&amp;t=h&amp;ll=30.307096,-81.603699&amp;spn=0.284546,0.439453&amp;z=11&amp;iwloc=A&amp;output=embed"></iframe>
+				</div>
+				<div id="footer">
+					<ul>
+						<li><span class="bold">Phone:</span> 904.281.2727</li>
+						<li><span class="bold">Fax:</span> 904.281.0444</li>
+						<li><span class="bold">Mailing Address:</span> CPAP Medical, 6950 Philips Highway #36, Jacksonville, FL 32216</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
